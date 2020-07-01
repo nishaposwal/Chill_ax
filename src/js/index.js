206b67b1ebf97c992a27a6de5c71300ac3ffae76
nisha poswal <nishaposwal321@gmail.com>
2020-07-02 00:30:32 +0530

    Add basic file folder

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var element = document.getElementById("menu");
+    element.classList.toggle("change");
+    var element1 = document.getElementById("humburger");
+    element1.classList.toggle("fas fa-close");
+
+ }
