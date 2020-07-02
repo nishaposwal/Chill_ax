8dfb5a64487b68b75510629dc052247b3bb60c25
nisha poswal <nishaposwal321@gmail.com>
2020-07-02 12:25:56 +0530

    Add work section

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 function myFunction() {
     var element = document.getElementById("menu");
     element.classList.toggle("change");
-    var element1 = document.getElementById("humburger");
-    element1.classList.toggle("fas fa-close");
+   
 
  }
